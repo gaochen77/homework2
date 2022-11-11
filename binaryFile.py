01f4cbcf7943487a44892face5836f5d7a10c2e3
gaochen77 <117835556+gaochen77@users.noreply.github.com>
2022-11-11 22:57:58 +0800

    Update binaryFile.py

--- binaryFile.py
+++ binaryFile.py
@@ -3,52 +3,46 @@ import shutil
 from util import strip_path
 
 
-def create_file(file_name):
-    file_name = strip_path(file_name)
-    is_exists = os.path.exists(file_name)
+def create_afile(filecode):
+    file = strip_path(filecode)
+    is_exists = os.path.exists(filecode)
     if not is_exists:
-        with open(file_name, mode='wb') as file:
-            file.write(bytes('hello world', 'ascii'))
+        with open(file, mode='wb') as file:
+            file.write(bytes('hello binary', 'ascii'))
             file.close()
-        print(file_name + ' created successfully')
+        print(filecode + ' created successfully')
         return True
     else:
-        print(file_name + ' file already exists')
+        print(filecode + ' filecode already exists')
         return False
 
-
-def delete_file(file_name):
-    file_name = strip_path(file_name)
-    is_exists = os.path.exists(file_name)
+def delete_file(filecode):
+    file_name = strip_path(filecode)
+    is_exists = os.path.exists(filecode)
     if not is_exists:
-        print(file_name + ' file does not exist')
+        print(filecode + ' filecode does not exist')
         return False
     else:
-        os.remove(file_name)
+        os.remove(filecode)
+    return True
+
+def move_file(o_filecode, n_filecode):
+    o_filecode = strip_path(o_filecode)
+    if not os.path.exists(o_filecode):
+        print(o_filecode + ' o_filecode does not exist')
+        return False
+    else:
+        shutil.move(o_filecode, n_filecode)
     return True
 
 
-# deleteDir(file_name)
-
-def move_file(old_file_name, new_file_name):
-    old_file_name = strip_path(old_file_name)
-    if not os.path.exists(old_file_name):
-        print(old_file_name + ' old file does not exist')
-        return False
-    else:
-        shutil.move(old_file_name, new_file_name)
-    return True
-
-
-def read_file(file_name):
-    file_name = strip_path(file_name)
-    is_exists = os.path.exists(file_name)
+def read_file(filecode):
+    filecode = strip_path(filecode)
+    is_exists = os.path.exists(filecode)
     if not is_exists:
-        print(file_name + ' file does not exist')
+        print(filecode + ' file does not exist')
         return
-    with open(file_name, 'rb') as file:
+    with open(filecode, 'rb') as file:
         file_content = file.read()
         file.close()
     return file_content
-
-# read_file('/Users/cp/Documents/pythontest/b.png')
